3edb203197946bc2076eb2592a3f4866fb29e45d
Casey Mullen <comullen97@gmail.com>
2023-01-26 15:03:10 -0800

    findTheRange

---
+++ challenge-find-the-range/findTheRange.js
@@ -0,0 +1,7 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+let highest = Math.max(...array);
+let lowest = Math.min(...array);
+console.log("Highest Number:" + highest);
+console.log("Lowest Number:" + lowest);
